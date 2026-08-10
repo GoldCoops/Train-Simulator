0abf8e8ce74a098e50a2d1298c8ce9d1f4d3cdb3
GoldCoops <nickpemberton1@pm.me>
2026-08-10 17:52:32 +1000

    added javafx via gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
+import sun.jvmstat.monitor.MonitoredVmUtil.mainClass
+
 plugins {
     id("java")
+    application
+    id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
 group = "com.trains"
@@ -9,10 +13,19 @@ repositories {
     mavenCentral()
 }
 
+javafx {
+    version = "25"
+    modules("javafx.controls", "javafx.fxml")
+}
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:6.0.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+application {
+    mainClass.set("com.trains.App")
 }
 
 tasks.test {
