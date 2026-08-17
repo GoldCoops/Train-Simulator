30ac28f57bdb7dd1ac0a43a7c97b9654cb18cbba
Mykhailo Chumak <mykhailo.chumak@students.mq.edu.au>
2026-08-17 20:51:10 +1000

    Add basic structure to SimulationView.java

--- src/main/java/com/trains/ui/SimulationView.java
+++ src/main/java/com/trains/ui/SimulationView.java
@@ -1,4 +1,22 @@
 package com.trains.ui;
 
-public class SimulationView {
+import java.util.Timer;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class SimulationView extends javax.swing.JFrame {
+    // GUI visual params
+    JFrame frame;
+    JPanel panel;
+    Timer screenUpdate; // Screen update timer
+    int screenUpdateDelay; // Initial delay (default 1 ms)
+    int screenSize; // Screen size in pixels in 4 directions e.g., 800x800
+
+    public SimulationView(int screenSize) {
+        this.screenSize = screenSize;
+        this.screenUpdateDelay = 1; // 1ms
+        this.frame = new JFrame();
+        this.panel = new JPanel();
+    }
 }
