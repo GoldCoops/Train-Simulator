725b4bcadbc74efd6e6adaa4bb14bee31737cf9d
gitjulia <julia.anderson130@icloud.com>
2026-08-19 21:54:39 +1000

    Updated App.java to display SimulationView.java

--- src/main/java/com/trains/App.java
+++ src/main/java/com/trains/App.java
@@ -9,13 +9,16 @@ import javafx.stage.Stage;
 
 
 public class App extends Application {
+    public static void main(String[] args) {
+        Simulation sim = new Simulation();
+        SimulationView view = new SimulationView(200);
+        SimulationController controller = new SimulationController();
+    }
 
+    // I don't think we need this anymore? Uses different library for visual, we seem to be using swing
     @Override
     public void start(Stage stage) throws Exception {
 
-        Simulation sim = new Simulation();
-        SimulationView view = new SimulationView();
-        SimulationController controller = new SimulationController();
 
 
     }
