b4c770be84617530ffc86228fcecf14697845959
Tailspin96 <103568017+Tailspin96@users.noreply.github.com>
2026-08-20 14:43:39 +1000

    refactor(app): move init to start method
    Main now only calls launch(args) as per JavaFX docs
    Move initialisation code to the JavaFX start()

--- src/main/java/com/trains/App.java
+++ src/main/java/com/trains/App.java
@@ -7,16 +7,17 @@ import javafx.application.Application;
 
 import javafx.stage.Stage;
 
-
 public class App extends Application {
-    public static void main(String[] args) {
-        Simulation sim = new Simulation();
-        SimulationView view = new SimulationView(200);
-        SimulationController controller = new SimulationController();
-    }
+	public static void main(String[] args) {
+		// Main should only contain this line
+		launch(args);
+	}
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-
-    }
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// All other init logic should be here
+		Simulation sim = new Simulation();
+		SimulationView view = new SimulationView(200);
+		SimulationController controller = new SimulationController();
+	}
 }
