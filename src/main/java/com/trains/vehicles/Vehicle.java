5d1e66130d07ef3e428be39e8ba5b8a6c90665d6
GoldCoops <nickpemberton1@pm.me>
2026-08-22 11:48:38 +1000

    fix(vehicle): make x and y coordinates double

--- src/main/java/com/trains/vehicles/Vehicle.java
+++ src/main/java/com/trains/vehicles/Vehicle.java
@@ -3,7 +3,7 @@ package com.trains.vehicles;
 import com.trains.network.*;
 
 public class Vehicle {
-    private int x = 0, y = 0;
+    private double x = 0, y = 0;
     // GridPos is a record, and as such the values inside are immutable, trains need to move, so we need something different.
     private boolean isStopped;
     private PathwaySegment curSegment;
