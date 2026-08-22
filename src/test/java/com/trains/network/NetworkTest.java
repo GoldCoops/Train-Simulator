735eeed340a82d1d26ee70139e7ccfb0eb8629e0
GoldCoops <nickpemberton1@pm.me>
2026-08-22 16:30:10 +1000

    test(network): add more test methods

--- src/test/java/com/trains/network/NetworkTest.java
+++ src/test/java/com/trains/network/NetworkTest.java
@@ -44,22 +44,56 @@ public class NetworkTest {
 
     @Test
     void isStationAtTest() {
-
+        network = new Network();
+        network.addNode(new GridPos(10, 20), STATION);
+        network.addNode(new GridPos(25, 20), STATION);
+        network.addNode(new GridPos(15, 20), NODE);
+        assertTrue(network.isStationAt(new GridPos(10, 20)));
+        assertTrue(network.isStationAt(new GridPos(25, 20)));
+        assertFalse(network.isStationAt(new GridPos(15, 20)));
+        assertFalse(network.isStationAt(new GridPos(100, 200)));
     }
 
     @Test
     void isStationTest() {
-
+        network = new Network();
+        Station station = new Station(new GridPos(10, 20), 10);
+        assertTrue(network.isStation(station));
+        assertFalse(network.isStation(new Node(new GridPos(10, 20))));
+        assertFalse(network.isStation(null));
     }
 
     @Test
     void isNoteAtTest() {
-
+        network = new Network();
+        network.addNode(new GridPos(10, 20), NODE);
+        network.addNode(new GridPos(25, 20), STATION);
+        network.addNode(new GridPos(15, 20), STATION);
+        network.addNode(new GridPos(95, 200), NODE);
+        assertTrue(network.isNodeAt(new GridPos(10, 20)));
+        assertTrue(network.isNodeAt(new GridPos(25, 20)));
+        assertTrue(network.isNodeAt(new GridPos(95, 200)));
+        assertTrue(network.isNodeAt(new GridPos(15, 20)));
+        assertFalse(network.isNodeAt(new GridPos(100, 200)));
     }
 
     @Test
     void connectNodesTest() {
-
+        network = new Network();
+        Node a = network.addNode(new GridPos(10, 20), NODE);
+        Node b = network.addNode(new GridPos(25, 20), STATION);
+        Node c = network.addNode(new GridPos(15, 20), STATION);
+        Node d = network.addNode(new GridPos(95, 200), NODE);
+        network.connectNodes(a, b);
+        network.connectNodes(c, d);
+        network.connectNodes(a, c);
+        assertEquals(3, network.getPathways().size());
+        assertEquals(b, a.getConnections().getFirst().getEnd());
+        assertEquals(a, b.getConnections().getFirst().getStart());
+        assertEquals(d, c.getConnections().getFirst().getEnd());
+        assertEquals(c, d.getConnections().getFirst().getStart());
+        assertEquals(c, a.getConnections().get(1).getEnd());
+        assertEquals(a, c.getConnections().get(1).getStart());
     }
 
     @Test
