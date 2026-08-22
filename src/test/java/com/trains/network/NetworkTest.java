8c339ebcf34ac8f8f46333e3f82c0b0bb88e8752
GoldCoops <nickpemberton1@pm.me>
2026-08-22 14:34:01 +1000

    test(Network): add addNodesTest and refactor

--- src/test/java/com/trains/network/NetworkTest.java
+++ src/test/java/com/trains/network/NetworkTest.java
@@ -1,35 +1,45 @@
 package com.trains.network;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static com.trains.network.Network.NodeType.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class NetworkTest {
-    private static Network network = new Network();
-
-
-    public static Network getNetwork() {
-        return network;
-    }
-
-    public static void setNetwork(Network network) {
-        NetworkTest.network = network;
-    }
+    private Network network;
 
     @Test
     void addNodeTest() {
-        network.addNode(new GridPos(10, 20), Network.NodeType.NODE);
+        network = new Network();
+        network.addNode(new GridPos(10, 20), NODE);
         assertEquals(1, network.getNodes().size());
         assertNotNull(network.getNodes().get(new GridPos(10, 20)));
-        network.addNode(new GridPos(15, 20), Network.NodeType.STATION);
+        network.addNode(new GridPos(15, 20), STATION);
         assertEquals(2, network.getNodes().size());
-        assertThrows(IllegalArgumentException.class, () -> network.addNode(new GridPos(10, 20), Network.NodeType.NODE)); // Node already at position
-        assertThrows(IllegalArgumentException.class, () -> network.addNode(new GridPos(10, 20), Network.NodeType.STATION));
+        assertThrows(IllegalArgumentException.class, () -> network.addNode(new GridPos(10, 20), NODE)); // Node already at position
+        assertThrows(IllegalArgumentException.class, () -> network.addNode(new GridPos(10, 20), STATION));
         assertEquals(2, network.getNodes().size());
     }
 
     @Test
     void addNodesTest() {
-
+        network = new Network();
+        HashMap<GridPos, Network.NodeType> nodes = new HashMap<>();
+        nodes.put(new GridPos(10, 20), NODE);
+        nodes.put(new GridPos(15, 20), STATION);
+        nodes.put(new GridPos(20, 20), STATION);
+        nodes.put(new GridPos(25, 20), NODE);
+        network.addNodes(nodes);
+        assertEquals(4, network.getNodes().size());
+        HashMap<GridPos, Network.NodeType> nodes1 = new HashMap<>();
+        nodes1.put(new GridPos(150, 220), STATION);
+        nodes1.put(new GridPos(10, 20), NODE);
+        nodes1.put(new GridPos(100, 200), NODE);
+        nodes1.put(new GridPos(15, 20), STATION);
+        assertThrows(IllegalArgumentException.class,() -> network.addNodes(nodes1)); // addNodes should bail and not add anything if any of the positions supplied are full.
+        assertEquals(4, network.getNodes().size());
     }
 
     @Test
