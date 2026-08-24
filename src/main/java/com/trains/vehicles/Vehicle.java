9258e9316c824c45bfbd68babadca81d3d8dc331
Mykhailo Chumak <mykhailo.chumak@students.mq.edu.au>
2026-08-24 11:19:28 +1000

    Add basic acceleration and deceleration handling methods in Train.java

--- src/main/java/com/trains/vehicles/Vehicle.java
+++ src/main/java/com/trains/vehicles/Vehicle.java
@@ -7,8 +7,8 @@ public class Vehicle {
     // GridPos is a record, and as such the values inside are immutable, trains need to move, so we need something different.
     private boolean isStopped;
     private PathwaySegment curSegment;
-    private int capacity; 
-    private float speed;
+    private int capacity;
+    protected float speed;
 
 
     public Vehicle(int x, int y, PathwaySegment curSegment) {
