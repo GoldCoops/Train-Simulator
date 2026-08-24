00026520e3e05af84fefb3b5efc517a0529ed04f
GoldCoops <nickpemberton1@pm.me>
2026-08-24 12:54:49 +1000

    fix: train + vehicle

--- src/main/java/com/trains/vehicles/Train.java
+++ src/main/java/com/trains/vehicles/Train.java
@@ -3,33 +3,10 @@ package com.trains.vehicles;
 import com.trains.network.PathwaySegment;
 
 public class Train extends Vehicle {
-    private float acceleration;
-    private float currentSpeed;
 
-    public Train(PathwaySegment curSegment, float maxSpeed) {
-        super(0,0,curSegment);
-        this.acceleration = .2f;
-        super.speed = maxSpeed;
-        this.currentSpeed = 0;
+
+    public Train(PathwaySegment curSegment, float maxSpeed, float acceleration) {
+        super(0,0,curSegment, maxSpeed, acceleration);
     }
 
-    /**
-     * Accelerates the train until it reaches its max speed value
-     */
-    private void accelerateTrain() {
-        while(super.speed > currentSpeed) {
-            currentSpeed += acceleration;
-        }
-    }
-    
-    /**
-     * Decelerates the train until it stops
-     */
-    private void decelerateTrain() {
-        while(currentSpeed > 0) {
-            currentSpeed -= acceleration;
-        }
-
-        currentSpeed = 0;
-    }
 }
