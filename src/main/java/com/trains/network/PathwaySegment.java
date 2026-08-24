2755a433941bb7d561c717da408399f6469bde6d
GoldCoops <nickpemberton1@pm.me>
2026-08-24 22:21:45 +1000

    refactor(Pathway): abstract distance calculation

--- src/main/java/com/trains/network/PathwaySegment.java
+++ src/main/java/com/trains/network/PathwaySegment.java
@@ -12,9 +12,7 @@ public class PathwaySegment {
     PathwaySegment(Node start, Node end) {
         this.start = start;
         this.end = end;
-        int xdist = Math.abs(start.getPos().x() - end.getPos().x());
-        int ydist = Math.abs(start.getPos().y() - end.getPos().y());
-        this.length = Math.sqrt(Math.pow(xdist, 2) + Math.pow(ydist,2));
+        this.length = start.getPos().distanceTo(end.getPos());
     }
 
 
