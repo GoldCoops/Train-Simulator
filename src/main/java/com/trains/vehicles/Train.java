168679a5aceafd86f5b8c2d7acb70d1d7265d328
GoldCoops <nickpemberton1@pm.me>
2026-08-24 20:48:34 +1000

    fix: update Train constructor

--- src/main/java/com/trains/vehicles/Train.java
+++ src/main/java/com/trains/vehicles/Train.java
@@ -1,12 +1,13 @@
 package com.trains.vehicles;
 
+import com.trains.cargo.CargoHold;
 import com.trains.network.PathwaySegment;
 
 public class Train extends Vehicle {
 
 
-    public Train(PathwaySegment curSegment, float maxSpeed, float acceleration) {
-        super(0,0,curSegment, maxSpeed, acceleration);
+    public Train(int x, int y, float maxSpeed, float acceleration, PathwaySegment curSegment, CargoHold cargoHold) {
+        super(x, y, maxSpeed, acceleration, curSegment, cargoHold);
     }
 
 }
