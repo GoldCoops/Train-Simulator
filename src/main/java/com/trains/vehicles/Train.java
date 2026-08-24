a136c34f55d17c9e7f00b4f5557a9ec7442f4521
Mykhailo Chumak <mykhailo.chumak@students.mq.edu.au>
2026-08-24 21:38:41 +1000

    Fix: dependency issue in Train.java

--- src/main/java/com/trains/vehicles/Train.java
+++ src/main/java/com/trains/vehicles/Train.java
@@ -2,12 +2,13 @@ package com.trains.vehicles;
 
 import com.trains.cargo.CargoHold;
 import com.trains.network.PathwaySegment;
+import com.trains.network.*;
 
 public class Train extends Vehicle {
 
 
-    public Train(int x, int y, float maxSpeed, float acceleration, PathwaySegment curSegment, CargoHold cargoHold) {
-        super(x, y, maxSpeed, acceleration, curSegment, cargoHold);
+    public Train(int x, int y, float maxSpeed, float acceleration, PathwaySegment curSegment, CargoHold cargoHold, Node targetNode) {
+        super(x, y, maxSpeed, acceleration, curSegment, cargoHold, targetNode);
     }
 
 }
