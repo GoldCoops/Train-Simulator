5b1300c58575895483c7442db44dd6bc0197af95
GoldCoops <nickpemberton1@pm.me>
2026-08-25 10:34:55 +1000

    refactor: change router constructor

--- src/main/java/com/trains/routing/Router.java
+++ src/main/java/com/trains/routing/Router.java
@@ -1,11 +1,13 @@
 package com.trains.routing;
 
+import com.trains.network.Network;
 import com.trains.network.Node;
 
 public class Router {
     // Handles pathfinding logic, I'm probably going to try using A* here, but if that fails I'll just use Dijkstra
-    public Router() {
-
+    private final Network network;
+    public Router(Network network) {
+        this.network = network;
     }
 
     public Route findRoute(Node origin, Node destination) {
