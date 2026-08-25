a24be911dca54b9827c53c6db5817ca51fe6b2e8
GoldCoops <nickpemberton1@pm.me>
2026-08-25 10:33:44 +1000

    feat: add Router class scaffold

--- src/main/java/com/trains/routing/Router.java
+++ src/main/java/com/trains/routing/Router.java
@@ -1,5 +1,14 @@
 package com.trains.routing;
 
+import com.trains.network.Node;
+
 public class Router {
     // Handles pathfinding logic, I'm probably going to try using A* here, but if that fails I'll just use Dijkstra
+    public Router() {
+
+    }
+
+    public Route findRoute(Node origin, Node destination) {
+        return null;
+    }
 }
