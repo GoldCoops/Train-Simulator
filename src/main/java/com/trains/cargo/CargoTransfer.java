e0a07e7e3a2013306b2b1d754b2d189bbded1909
GoldCoops <nickpemberton1@pm.me>
2026-08-25 11:06:54 +1000

    docs: add CargoTransfer description

--- src/main/java/com/trains/cargo/CargoTransfer.java
+++ src/main/java/com/trains/cargo/CargoTransfer.java
@@ -2,4 +2,6 @@ package com.trains.cargo;
 
 public class CargoTransfer {
     // This class should manage the boarding removal of cargo from vehicles and stations to ensure no desync between their separate cargo tracking lists
+    // This class should ensure that no cargo is lost, when moving cargo between two holds, we should first add the cargo to the new list
+    // then confirm it exists in the new list, then remove it from the old list.
 }
