5fed74b98a47f689211c685fe6dcd3ef63d994fa
aanya-21 <aanya.chaudhary@students.mq.edu.au>
2026-08-28 00:34:57 +1000

    feat(cargoSpawner): add automatic passenger spawning

--- src/main/java/com/trains/sim/CargoSpawner.java
+++ src/main/java/com/trains/sim/CargoSpawner.java
@@ -1,9 +1,10 @@
 package com.trains.sim;
 import com.trains.cargo.*;
 import com.trains.network.*;
-
-
 import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public final class CargoSpawner {
     private final Network network;
@@ -28,6 +29,46 @@ public final class CargoSpawner {
         Cargo passenger = Cargo.passenger(destination.getPos());
 
         return CargoTransfer.insertCargo(origin.getCargoHold(), passenger);
+    }
+
+    /**
+     * Get all current stations in the network
+     * @return a list containing all the stations in the network
+     */
+
+    private List<Station> getStation(){
+        List<Station> stations = new ArrayList<>();
+
+        for(Node node: network.getNodes().values()){
+            if (node instanceof Station){
+                stations.add((Station) node);
+            }
+        }
+
+        return stations;
+    }
+
+    /**
+     * Spawn the passenger using stations from that network
+     * @return true if the passenger was siccessfully spawned
+     */
+    public boolean spawnPassenger(){
+        List<Station> stations = getStation();
+
+        if (stations.size() < 2){
+            return false;
+        }
+
+        Random random = new Random();
+
+        Station origin = stations.get(random.nextInt(stations.size()));
+
+        Station destination;
+        do{
+            destination = stations.get(random.nextInt(stations.size()));
+        } while (destination == origin);
+
+        return spawnPassenger(origin, destination);
     }
 
 }
