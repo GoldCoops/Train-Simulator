e104d50202947c81de552814e008a11ac3097eb9
GoldCoops <nickpemberton1@pm.me>
2026-09-18 10:17:48 +1000

    docs: update routing docs

--- src/main/java/com/trains/routing/package-info.java
+++ src/main/java/com/trains/routing/package-info.java
@@ -1,6 +1,9 @@
 /**
- * The Routing Package, contains:
+ * The Routing Package
+ * <p>
+ * contains:
  * 1. Route - A Static route which just contains the directions to get to a certain node
  * 2. Router - The Class responsible for pathfinding and making a Route.
+ * </p>
  */
 package com.trains.routing;
